ff319682f190bd5b2321ba23a50a7695aa88460b
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-02 20:55:18 +0200

    Register project portfolio documentation in in-app help

--- taxonomy-app/src/main/java/com/taxonomy/shared/controller/HelpController.java
+++ taxonomy-app/src/main/java/com/taxonomy/shared/controller/HelpController.java
@@ -60,6 +60,8 @@ public class HelpController {
         new String[]{"USER_GUIDE",               "📖", "help.toc.USER_GUIDE",                    "help.audience.everyone"},
         new String[]{"CONCEPTS",                  "💡", "help.toc.CONCEPTS",                      "help.audience.everyone"},
         new String[]{"EXAMPLES",                  "📝", "help.toc.EXAMPLES",                      "help.audience.everyone"},
+        new String[]{"PROJECT_REQUIREMENT_PORTFOLIO", "🧭", "help.toc.PROJECT_REQUIREMENT_PORTFOLIO", "help.audience.everyone"},
+        new String[]{"PROJECT_PORTFOLIO_GIT_COLLABORATION", "🤝", "help.toc.PROJECT_PORTFOLIO_GIT_COLLABORATION", "help.audience.developers"},
         new String[]{"FRAMEWORK_IMPORT",          "📥", "help.toc.FRAMEWORK_IMPORT",              "help.audience.everyone"},
         new String[]{"GIT_INTEGRATION",           "🔀", "help.toc.GIT_INTEGRATION",               "help.audience.developers"},
         new String[]{"JGIT_STORAGE_HIBERNATE",    "🗃️", "help.toc.JGIT_STORAGE_HIBERNATE",        "help.audience.devops"},
