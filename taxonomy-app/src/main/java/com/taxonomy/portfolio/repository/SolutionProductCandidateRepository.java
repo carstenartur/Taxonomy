514e71b7304825966ee33c9a55f16933fec42b54
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-04 01:29:50 +0200

    Add batch product candidate query

--- taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/SolutionProductCandidateRepository.java
+++ taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/SolutionProductCandidateRepository.java
@@ -5,13 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface SolutionProductCandidateRepository
         extends JpaRepository<SolutionProductCandidate, Long> {
 
-    List<SolutionProductCandidate> findByProjectSolutionIdOrderByCoveragePercentDesc(Long projectSolutionId);
+    List<SolutionProductCandidate> findByProjectSolutionIdOrderByCoveragePercentDesc(
+            Long projectSolutionId);
+
+    List<SolutionProductCandidate>
+            findByProjectSolutionIdInOrderByProjectSolutionIdAscCoveragePercentDesc(
+                    Collection<Long> projectSolutionIds);
 
     Optional<SolutionProductCandidate> findByProjectSolutionIdAndProductId(
             Long projectSolutionId, Long productId);
