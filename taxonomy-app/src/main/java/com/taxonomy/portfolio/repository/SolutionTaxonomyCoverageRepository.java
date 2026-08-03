e9190cc9a1bd1c438471e64ce4a7ebf1a2b5ff4b
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-04 01:29:20 +0200

    Add batch solution coverage queries

--- taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/SolutionTaxonomyCoverageRepository.java
+++ taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/SolutionTaxonomyCoverageRepository.java
@@ -3,6 +3,7 @@ package com.taxonomy.portfolio.repository;
 import com.taxonomy.portfolio.model.SolutionTaxonomyCoverage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,7 +12,12 @@ public interface SolutionTaxonomyCoverageRepository
 
     List<SolutionTaxonomyCoverage> findBySolutionIdOrderByNodeCodeAsc(Long solutionId);
 
+    List<SolutionTaxonomyCoverage> findBySolutionIdInOrderBySolutionIdAscNodeCodeAsc(
+            Collection<Long> solutionIds);
+
     List<SolutionTaxonomyCoverage> findByNodeCode(String nodeCode);
+
+    List<SolutionTaxonomyCoverage> findByNodeCodeIn(Collection<String> nodeCodes);
 
     Optional<SolutionTaxonomyCoverage> findBySolutionIdAndNodeCode(Long solutionId, String nodeCode);
 }
