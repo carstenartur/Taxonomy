1a26fb36d7b9582bd5c92dce49d3d4c28b5c0789
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-03 06:15:20 +0200

    Count open project conflicts without loading entities

--- taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/ProjectConflictRepository.java
+++ taxonomy-app/src/main/java/com/taxonomy/portfolio/repository/ProjectConflictRepository.java
@@ -1,14 +1,18 @@
 package com.taxonomy.portfolio.repository;
 
+import com.taxonomy.portfolio.model.PortfolioTypes.ConflictStatus;
 import com.taxonomy.portfolio.model.ProjectConflict;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProjectConflictRepository extends JpaRepository<ProjectConflict, Long> {
 
     List<ProjectConflict> findByProjectIdOrderByConfidenceDescDetectedAtDesc(Long projectId);
+
+    long countByProjectIdAndStatusNotIn(Long projectId, Collection<ConflictStatus> statuses);
 
     Optional<ProjectConflict> findByIdAndProjectId(Long id, Long projectId);
 
