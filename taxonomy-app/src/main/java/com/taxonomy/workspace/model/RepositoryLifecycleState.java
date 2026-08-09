2ffae671ca6f42e0d26407bc9f7a59b3862970f2
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-09 18:29:34 +0200

    Represent repository provisioning explicitly

--- taxonomy-app/src/main/java/com/taxonomy/workspace/model/RepositoryLifecycleState.java
+++ taxonomy-app/src/main/java/com/taxonomy/workspace/model/RepositoryLifecycleState.java
@@ -2,6 +2,7 @@ package com.taxonomy.workspace.model;
 
 /** Provisioning/lifecycle state of a central architecture repository. */
 public enum RepositoryLifecycleState {
+    PROVISIONING,
     ACTIVE,
     ARCHIVED,
     DELETING,
