3237eb4cb36d7050646b56c5fb86f922a7dc123e
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-29 10:30:45 +0200

    Enforce cleanup authorization at the job gate

--- taxonomy-build/src/test/java/com/taxonomy/build/WorkflowRunCleanupTriggerPolicyTest.java
+++ taxonomy-build/src/test/java/com/taxonomy/build/WorkflowRunCleanupTriggerPolicyTest.java
@@ -15,13 +15,28 @@ class WorkflowRunCleanupTriggerPolicyTest {
                 ".github/workflows/cleanup-workflow-runs.yml"));
 
         assertThat(workflow)
-                .contains("issue_comment:")
-                .contains("types: [created]")
-                .contains("github.event.comment.user.login == github.repository_owner")
-                .contains("github.event.comment.body == '/actions-cleanup'")
-                .contains("github.event_name != 'issue_comment'")
-                .contains("DRY_RUN: ${{ github.event_name == 'workflow_dispatch'"
+                .contains("issue_comment:\n    types: [created]")
+                .doesNotContain("  issues: read");
+        assertThat(cleanupJobGate(workflow)).isEqualTo("""
+                cleanup:
+                    if: >-
+                      github.event_name != 'issue_comment' ||
+                      (github.event.comment.user.login == github.repository_owner &&
+                       github.event.comment.body == '/actions-cleanup')
+                """);
+        assertThat(workflow).contains(
+                "DRY_RUN: ${{ github.event_name == 'workflow_dispatch'"
                         + " && inputs.dry_run || false }}");
+    }
+
+    private static String cleanupJobGate(String workflow) {
+        int jobStart = workflow.indexOf("\n  cleanup:\n");
+        int runsOn = workflow.indexOf("\n    runs-on:", jobStart);
+        if (jobStart < 0 || runsOn < 0) {
+            throw new IllegalArgumentException(
+                    "cleanup job and its runs-on boundary must exist");
+        }
+        return workflow.substring(jobStart + 3, runsOn + 1);
     }
 
     private static Path repositoryRoot() {
