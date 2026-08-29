0ed56e15289b675206dc08a49f04d43b54a7103b
Carsten Hammer <carsten.hammer@t-online.de>
2026-08-29 08:52:06 +0200

    Limit retention contract test to GNU/Linux

--- taxonomy-build/src/test/java/com/taxonomy/build/WorkflowRunRetentionPolicyTest.java
+++ taxonomy-build/src/test/java/com/taxonomy/build/WorkflowRunRetentionPolicyTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@EnabledOnOs({OS.LINUX, OS.MAC})
+@EnabledOnOs(OS.LINUX)
 class WorkflowRunRetentionPolicyTest {
 
     @Test
