90c15f2ed193dc6891e7767f191477c917c36e24
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-10 10:32:11 +0200

    fix(help): register module-boundary documentation

--- taxonomy-app/src/main/java/com/taxonomy/shared/controller/HelpController.java
+++ taxonomy-app/src/main/java/com/taxonomy/shared/controller/HelpController.java
@@ -77,6 +77,7 @@ public class HelpController {
         new String[]{"API_REFERENCE",             "🔌", "help.toc.API_REFERENCE",                 "help.audience.integrators"},
         new String[]{"CURL_EXAMPLES",             "💻", "help.toc.CURL_EXAMPLES",                 "help.audience.integrators"},
         new String[]{"ARCHITECTURE",              "🏗️", "help.toc.ARCHITECTURE",                  "help.audience.developers"},
+        new String[]{"MODULE_BOUNDARIES",         "🧱", "help.toc.MODULE_BOUNDARIES",             "help.audience.developers"},
         new String[]{"ARCHITECTURE_EDITOR",       "✏️", "help.toc.ARCHITECTURE_EDITOR",           "help.audience.everyone"},
         new String[]{"DECISION_PIPELINE",         "🔬", "help.toc.DECISION_PIPELINE",             "help.audience.developers"},
         new String[]{"DECISION_RATIONALE_REPORT", "🧭", "help.toc.DECISION_RATIONALE_REPORT",     "help.audience.everyone"},
