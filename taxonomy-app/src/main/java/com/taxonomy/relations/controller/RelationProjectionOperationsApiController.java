2a6c2e69b3744b38ac486eabe629ca2232fb4aaf
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-11 09:21:59 +0200

    refactor(relations): consume foundation projection preconditions

--- taxonomy-app/src/main/java/com/taxonomy/relations/controller/RelationProjectionOperationsApiController.java
+++ taxonomy-app/src/main/java/com/taxonomy/relations/controller/RelationProjectionOperationsApiController.java
@@ -1,6 +1,7 @@
 package com.taxonomy.relations.controller;
 
 import com.taxonomy.dsl.storage.ExpectedHeadDslCommitter.BranchHeadConflictException;
+import com.taxonomy.http.GitHttpPrecondition;
 import com.taxonomy.relations.service.RelationBranchProjectionRebuildService.BranchProjectionSourceException;
 import com.taxonomy.relations.service.RelationProjectionOperationsService;
 import com.taxonomy.relations.service.RelationProjectionOperationsService.ProjectionStatus;
