fb1f23ff5b043b06bdef1cc56be7c763819f434b
Carsten Hammer <carsten.hammer@t-online.de>
2026-09-11 09:20:26 +0200

    refactor(relations): consume foundation Git HTTP preconditions

--- taxonomy-app/src/main/java/com/taxonomy/relations/controller/GitRelationCommandApiController.java
+++ taxonomy-app/src/main/java/com/taxonomy/relations/controller/GitRelationCommandApiController.java
@@ -4,6 +4,7 @@ import com.taxonomy.dsl.command.ArchitectureRelationDslTransformer.ChangeKind;
 import com.taxonomy.dsl.command.ArchitectureRelationDslTransformer.RelationDefinition;
 import com.taxonomy.dsl.command.ArchitectureRelationDslTransformer.RelationIdentity;
 import com.taxonomy.dsl.storage.ExpectedHeadDslCommitter.BranchHeadConflictException;
+import com.taxonomy.http.GitHttpPrecondition;
 import com.taxonomy.model.RelationType;
 import com.taxonomy.relations.command.ArchitectureRelationGitCommandService.CommandMetadata;
 import com.taxonomy.relations.command.ArchitectureRelationGitCommandService.CommandResult;
